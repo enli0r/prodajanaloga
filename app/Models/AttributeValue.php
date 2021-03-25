95b0dc1be01d144334928fa03825a2951cd5f546
enli0r <milanniko19@gmail.com>
2021-03-25 14:13:02 +0100

    Added AttributeValue model and migration

---
+++ app/Models/AttributeValue.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AttributeValue extends Model
+{
+    use HasFactory;
+
+    protected $table = 'attribute_values';
+
+    protected $fillable = ['attribute_value', 'attribute_id'];
+    
+    public function attribute(){
+        return $this->belongsTo(Attribute::class, 'attribute_id');
+    }
+    
+}
